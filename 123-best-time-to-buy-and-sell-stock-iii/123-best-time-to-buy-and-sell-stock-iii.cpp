ead124f6bbb4197708226f832923b735ba560b58
Sanyam Mehendiratta <53525430+Sanyam2000-dot@users.noreply.github.com>
2022-03-24 14:45:21 +0530

    Time: 1021 ms (5.00%), Space: 218 MB (5.41%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxProfit( vector<int>& prices) {
+        unordered_map<string, int> memo;
+        vector<vector<vector<int>>> mp(prices.size(),vector<vector<int>>(2,vector<int>(2+1,-1)));
+        
+        return maxProfits(prices, 0,1, 2, memo, mp);
+    }
+    int maxProfits(vector<int> &prices, int currentDay, int canBuy, int k, unordered_map<string, int> &memo,  vector<vector<vector<int>>> &mp ){
+        if(currentDay >= prices.size() || k ==0)
+            return 0;
+        
+        if(mp[currentDay][canBuy][k] != -1)
+            return mp[currentDay][canBuy][k];
+        // string currentKey = to_string(currentDay)+ "-"+ to_string(canBuy) + "-"+ to_string(transaction);
+        // if(memo.find(currentKey) != memo.end())
+        //     return memo[currentKey];
+        int ans =0;
+        if(canBuy ==1){
+            int idle = maxProfits(prices, currentDay+1, canBuy, k, memo, mp);
+        int buy = -prices[currentDay] + maxProfits(prices, currentDay+1,0, k, memo, mp);
+            ans = max(idle, buy);
+        }else{
+            int idle = maxProfits(prices, currentDay+1, canBuy, k, memo, mp );
+        int sell = prices[currentDay] + maxProfits(prices, currentDay+1,1, k-1, memo, mp);
+        ans= max(idle, sell);
+        }
+        // memo[currentKey] = ans;
+        mp[currentDay][canBuy][k] = ans;
+        return mp[currentDay][canBuy][k];
+        }
+        
+};
