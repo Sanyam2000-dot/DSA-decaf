e9eb5f46351993cdcf5b9dbe79cf0006f60988b9
Sanyam Mehendiratta <53525430+Sanyam2000-dot@users.noreply.github.com>
2022-04-06 17:21:50 +0530

    Time: 3 ms (95.50%), Space: 10.3 MB (63.75%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -10,14 +10,14 @@ public:
     vector<int> common(vector<int>& nums1, vector<int>& nums2, unordered_map<int, bool> &memo1){
         
         for(auto i:nums1){
-            
-                memo1[i] =   true;
+            if(memo1.find(i) == memo1.end()){
+                memo1[i] =   true;}
             
         }
         vector<int> ans;
         
     for(auto i:nums2){
-            if(memo1.find(i) != memo1.end() && memo1[i]==true ){
+            if(memo1.find(i) != memo1.end() && memo1[i] ){
                 ans.push_back(i);
                 memo1[i] = false;
             }
